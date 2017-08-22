36c8a41b58caa421d02225ca4ef8d03344c32cdb
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-08-22 10:29:50 +0700

    removed empty files, fixed indentation and replaced the static fields with dynamic fields

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,23 +3,23 @@ class ChargesController < ApplicationController
 	end
 
 	def create 
-	  # Amount in cents
-	  @amount = 500
+		# Amount in cents
+		@amount = 500
 
-	  customer = Stripe::Customer.create(
-	    :email => params[:stripeEmail],
-	    :source  => params[:stripeToken]
-	  )
+		customer = Stripe::Customer.create(
+		:email => params[:stripeEmail],
+		:source  => params[:stripeToken]
+		)
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
+		charge = Stripe::Charge.create(
+		:customer    => customer.id,
+		:amount      => @amount,
+		:description => 'Rails Stripe customer',
+		:currency    => 'usd'
+		)
 
 	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to new_charge_path
+		flash[:error] = e.message
+		redirect_to new_charge_path
 	end
 end
